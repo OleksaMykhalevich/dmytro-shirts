3ff2d9a1012691fa8b88a5d3c9b46836164dc367
Oleksiy <oleksiymykhalevich86@gmail.com>
2022-09-09 16:11:32 +0200

    ///

--- src/Components/pages/HomePage.js
+++ src/Components/pages/HomePage.js
@@ -1,14 +1,18 @@
 import React from "react";
-import homePreviewImg from "../../Components/pages/pagesImg/words.jpg";
+import homePreviewImg from "../../Components/pages/pagesImg/words1.jpg";
 import "./HomePage.css";
 import { tShirtsArray } from "../shared/tShirtsArray";
 import { ShirtElement } from "../Elements/shirtElement";
 import { Grid } from "@mui/material";
+import backgroundImgCar from "./pagesImg/0317dfa27223471ac1a44d76d61b8f65.jpg";
 
 export const HomePage = () => {
   return (
     <>
       <div className="preview">
+        <div className="backgroundImg">
+          <img alt="backgroundImgCar" src={backgroundImgCar}></img>
+        </div>
         <div className="title">
           <img alt="homePreviewImg" src={homePreviewImg}></img>
         </div>
